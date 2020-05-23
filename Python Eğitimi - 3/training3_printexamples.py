577df1d9baa1372ff5360413a6a22c52f66c49d6
csaglar <49249712+csaglar@users.noreply.github.com>
2020-05-23 15:56:06 +0300

    Create training3_printexamples.py

---
+++ Python Eğitimi - 3/training3_printexamples.py
@@ -0,0 +1,25 @@
+#Examples 1
+
+print("Hello World")
+print("Hello","World")
+
+#Examples 2
+
+print('Hello' + 'World')
+print("Hello " + "World")
+print("Hello" + " " + "World")
+
+#Examples 3
+
+print("Hello World" * 3)
+print("Hello World " * 3)
+print(("Hello World " + " ")*3)
+
+#Examples 4
+print("23" + "7")
+print(23 + 7)
+
+#Examples 5
+print("19 Mayıs 1919")
+print("19" , "Mayıs" , "1919")
+print(19 , "Mayıs" , 1919)
