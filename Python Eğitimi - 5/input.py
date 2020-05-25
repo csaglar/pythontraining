c4607a503fb8f07e2a03e5fbaf8bd0e7c47065e9
csaglar <49249712+csaglar@users.noreply.github.com>
2020-05-25 13:14:45 +0300

    Create input.py

---
+++ Python Eğitimi - 5/input.py
@@ -0,0 +1,7 @@
+#EvdeKalSaglıklaKal
+
+ad = input("Adınızı Girin:")
+soyad = input("Soyadınızı Girin:")
+dogum_yili =int(input("Doğum Yılınızı Yazın:") )
+
+print(ad,soyad,2020-dogum_yili,"yaşındasınız.")
